8b8d4396c71f1e2f582c503381278af16da6da11
nadilsandaruwan97@gmail.com <87482209+Nadil97@users.noreply.github.com>
2022-10-25 21:08:37 +0530

    add file

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:comment/secondroute.dart';
+import 'package:flutter/material.dart';
+import 'package:comment_box/comment/comment.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('home page'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Comment'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
